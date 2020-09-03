bdd37bc70b80a808aac64ce1a018f9607735c7e2
hollyollyoxenfree <66744578+hollyollyoxenfree@users.noreply.github.com>
2020-09-03 15:29:18 -0700

    Update util.js

--- util.js
+++ util.js
@@ -13,7 +13,7 @@ function setElement(key, value){
 }
 
 function getAuthToken(isConfig){
-    console.log("Getting authorization token.");
+    console.log("Requesting an authorization token.");
     if("" != getItem("tenantid") && "" != getItem("clientid") && "" != getItem("secret"))
     {
         const xhr = new XMLHttpRequest();
@@ -23,11 +23,10 @@ function getAuthToken(isConfig){
         // open request
         xhr.open("POST", authurl);
 
-         // set headers
-         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        // set headers
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
         // send callout
-        console.log("Sending request.\n" +"tennant: " +  getItem("tenantid")+ "\nclient: " + getItem("clientid")+ "\necret: " +   getItem("secret"));
         xhr.send(formdata);
 
         // handle response
@@ -40,17 +39,17 @@ function getAuthToken(isConfig){
                 // set token expiration (res.expires_in converted from seconds from now to miliseconds)
                 setItem("exp", Date.now() + (res.expires_in * 1000));
                 
-                console.log("Get authorization token- success!")
+                console.log("Success!")
                 if(isConfig){
                     document.getElementById("output").innerHTML = "Configuration values verified and saved. You may close this window.";
                     document.getElementById("warning").innerHTML = "\n";
                 }
                 else{
-                    buildJSON();
+                    parseIOCs();
                 }
             }
             else{
-                console.log("Get authorization token failed. \nStatus Code: " + xhr.status +  "\nResponse text: " + xhr.responseText);
+                console.log("Request for authorization token failed. \nStatus Code: " + xhr.status +  "\nResponse text: " + xhr.responseText);
                 document.getElementById("output").innerHTML = "\n";
                 document.getElementById("warning").innerHTML = "Authorization Error. Please check configuration values, console logs, and application permissions.";
                 setItem("authtoken", "");
